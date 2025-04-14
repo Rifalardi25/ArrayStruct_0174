e041114f36c76991007b82f2ec9b38eb80734504
RifalArdi25 <rifalardi25@gmail.com>
2025-04-14 18:23:50 +0700

    Commit tempCodeRunner

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+string matkul[] = {"Pemrograman", "Algoritma"};
+
+float nilaiAkhir[2] = {1, 2.5};
+
+char grade[5];
+
+int main()
+{
+    cout << "Nama Matakuliah : " << matkul[1] << endl;
+    matkul[0] = "Logika Teknik Pemrograman";
+    cout << "Nama Matakuliah : " << matkul[0] << endl;
+
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Nilai akhir ke-" << i + 1 << "=" << nilaiAkhir[i] << endl;
+    }
+
+    cout << "Input Grade" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Masukkan Grade : ";
+        cin >> grade[i];
+    }
+
+    cout << endl;
+    cout << "Tampilkan Grade" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Grade ke-" << i + 1 << grade[i] << endl;
+    }
+}
